cf561c089f9443e4f660b0c63f15afd416dbc023
21harley <john.llanes.1996@gmail.com>
2022-05-26 16:43:15 -0400

    init

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,151 @@
+function Home() {
+  return (
+    <div className="container-home">
+      <div className="container-home__grid">
+        <div className="container-home--none">
+          <div className="banner-cube banner-cube--margin"></div>
+          <div className="banner-cube banner-cube--margin"></div>
+        </div>
+        <div>
+          <div className="logo"></div>
+          <div className="banner-large banner-large--margen"></div>
+          <div className="container-text">
+            <h1 className="container-text__title">¿Cara o Cruz?</h1>
+            <p className="container-text__p">
+              ¿Quieres <b>jugar cara o cruz</b> para probar tu suerte o para
+              ayudarte a tomar una decisión? ¡Genial! Has llegado al sitio
+              indicado.
+            </p>
+            <p className="container-text__p">
+              ¡Genial! Has llegado al sitio indicado.
+            </p>
+            <p className="container-text__p">
+              Aquí podrás hacer el <b>lanzamiento de moneda virtual</b> de forma
+              sencilla y divertida.¡Prueba tu suerte!
+            </p>
+          </div>
+          <div className="container-moneda"></div>
+          <div className="button-moneda"></div>
+          <div className="container-home--block">
+            <div className="banner-cube banner-cube--margin"></div>
+            <div className="static-box"></div>
+          </div>
+          <div className="container-text">
+            <h2 className="container-text__h2">
+              ¿Qué es el juego de Cara o Cruz?
+            </h2>
+            <p className="container-text__p">
+              Se trata de un juego de azar en el cual dos jugadores{" "}
+              <b>lanzan una moneda de metal al aire</b> e intentan adivinar de
+              qué lado caerá ésta, si en el lado de la cara o de la cruz.
+              <b>
+                La persona que predice el resultado correcto es la que gana el
+                juego
+              </b>
+              .
+            </p>
+            <p className="container-text__p">
+              En algunos países éste juego se conoce como:{" "}
+              <b>un volado, echar la suerte, o jugar al águila o sol</b> (por
+              las características de la moneda oficial del país). El juego de
+              Cara o cruz es muy divertido y apto para personas de todas las
+              edades, y lo mejor de todo es que se puede jugar con cualquier
+              tipo de moneda.
+            </p>
+          </div>
+          <div className="banner-large"></div>
+          <div className="container-text">
+            <h2 className="container-text__h2">
+              ¿Cómo jugar Cara o Cruz Online?
+            </h2>
+            <p className="container-text__p">
+              Es muy probable que en algún momento quieras apostar algo con un
+              amigo o tengas que tomar una decisión y quieras echar un volado
+              para saber qué hacer, pero no tengas una moneda a la mano.
+            </p>
+            <p>
+              ¡No te preocupes! En nuestro sitio podrás{" "}
+              <b>lanzar una moneda virtual al aire</b> para así tomar la mejor
+              decisión. ¡Es muy fácil!, sólo tendrás que hacer lo siguiente:
+            </p>
+            <ol>
+              <li className="container-text__li">
+                Solo necesitas ponerte de acuerdo con la persona contra quien
+                estés jugando para elegir quien escoge cara y quien escoge cruz.
+              </li>
+              <li className="container-text__li">
+                Da click al botón de "<b>Lanzar Moneda</b>". En ese momento
+                nuestro simulador procederá a lanzar una moneda virtual al aire.
+                Mientras ves y escuchas como ésta gira, como una moneda de metal
+                de verdad, tu suerte estará literalmente en el aire.
+              </li>
+              <li className="container-text__li">
+                Una vez que la moneda virtual deje de girar y "caiga", podrás
+                observar si elegiste la opción correcta: <b>cara o cruz</b>.
+              </li>
+              <li className="container-text__li">
+                Puedes volver a lanzar la moneda al aire cuantas veces quieras
+                en caso de que hayas acordado jugar 2 de 3, o 3 de 5.
+              </li>
+            </ol>
+          </div>
+          <div className="banner-large"></div>
+          <div className="container-text">
+            <h2 className="container-text__h2">
+              ¿Cuáles son las probabilidades de ganar al lanzar una moneda?
+            </h2>
+            <p className="container-text__p">
+              Las probabilidades de{" "}
+              <b>ganar un volado con una moneda son del 50%</b>. Esto significa
+              que por cada dos veces que lances la moneda, deberías esperar
+              ganar una vez y perder otra. Por supuesto, esto depende
+              completamente del azar por lo que pueden haber ocasiones en las
+              que tengas una buena racha y puedas ganar (o perder) de forma
+              continua.
+            </p>
+            <p className="container-text__p">
+              Es importante recordar que{" "}
+              <b>el lanzar una moneda al aire es un juego de azar</b>, por lo
+              que nadie puede predecir con exactitud el resultado en todos los
+              lanzamientos. El objetivo del juego es intentar{" "}
+              <b>
+                superar las probabilidades y ganar la mayor cantidad de veces
+              </b>
+              .
+            </p>
+          </div>
+          <div className="banner-large"></div>
+          <div className="container-text">
+            <h2 className="container-text__h2">
+              ¿Para qué sirve echar un volado online?
+            </h2>
+            <p className="container-text__p">
+              Existen ocasiones en las que debes tomar una decisión y elegir
+              entre dos posibilidades sin que ninguna de ellas te resulte
+              particularmente atractiva. En ese momento dejar esa decisión a la
+              suerte y <b>echar un volado con una moneda</b>, y si no la tienes,
+              puedes echar un <b>volado online</b> aquí en nuestro sitio.
+            </p>
+            <p className="container-text__p">
+              Los volados son muy comunes y divertidos cuando quieres apostar
+              con tus amigos o cuando vas a jugar un partido de soccer,
+              voleibol, béisbol, o fútbol americano y necesitas decidir qué
+              equipo empieza con la posición de la pelota.
+            </p>
+          </div>
+        </div>
+        <div className="container-home--none">
+          <div className="banner-cube banner-cube--margin"></div>
+          <div className="static-box"></div>
+        </div>
+      </div>
+      <div className="logo logo--margin-footer"></div>
+      <div className="footer">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. footer
+      </div>
+    </div>
+  );
+}
+
+export default Home;
